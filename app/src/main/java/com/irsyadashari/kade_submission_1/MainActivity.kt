7d0d55c1f6450662a898e4e9ca9764e8c9e333da
irsyad <irsyad.ashari17@gmail.com>
2019-10-06 16:04:25 +0800

    Adding Project KADE Submission 1

---
+++ app/src/main/java/com/irsyadashari/kade_submission_1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.irsyadashari.kade_submission_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
